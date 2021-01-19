1fed529e95e01c341289640ec65face7674fd436
demaceo <hdemaceo@gmail.com>
2021-01-19 12:13:33 -0700

    Create testing suite for App

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,1 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { render, screen, waitFor } from "@testing-library/react";
+import App from "./App";
+import "@testing-library/jest-dom";
+import { getOrders, makeOrders } from "../../apiCalls";
+import { orders } from "../../testingData";
+
+describe("App", () => {
+    // beforeEach(() => {
+    // jest.mock("../../apiCalls", () => ({
+    //     getOrders: jest.fn(),
+    // }));
+    // const mockedOrders = getOrders();
+// });
+    it("renders without crashing", () => {
+      const div = document.createElement("div");
+      ReactDOM.render(<App />, div);
+      ReactDOM.unmountComponentAtNode(div);
+    });
+    it("should render a header title", () => {
+        render(<App />);
+        const headerTitle = document.getElementById("title");
+
+        expect(headerTitle).toBeInTheDocument()
+    }); 
+});
 
