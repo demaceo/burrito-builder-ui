2454880a96ad5dbefa3b8645974de949a84b07f6
demaceo <hdemaceo@gmail.com>
2021-01-19 11:15:44 -0700

    create makeOrder POST request function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,3 +3,17 @@ export const getOrders = () => {
       .then(response => response.json())
       .catch((err) => console.log(err))
 }
+
+export const makeOrder = (order) => {
+  return fetch('http://localhost:3001/api/v1/orders', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(order),
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json))
+    .catch((err) => console.log(err));
+};
+
