7425872a5fac61d5eea4e9373ff886fc26428ba7
demaceo <hdemaceo@gmail.com>
2021-01-19 12:25:19 -0700

    Add async test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -5,14 +5,12 @@ import App from "./App";
 import "@testing-library/jest-dom";
 import { getOrders, makeOrders } from "../../apiCalls";
 import { orders } from "../../testingData";
+jest.mock("../../apiCalls.js");
 
 describe("App", () => {
-    // beforeEach(() => {
-    // jest.mock("../../apiCalls", () => ({
-    //     getOrders: jest.fn(),
-    // }));
-    // const mockedOrders = getOrders();
-// });
+   beforeEach(() => {
+     getOrders.mockResolvedValueOnce(orders);
+   });
     it("renders without crashing", () => {
       const div = document.createElement("div");
       ReactDOM.render(<App />, div);
@@ -24,5 +22,15 @@ describe("App", () => {
 
         expect(headerTitle).toBeInTheDocument()
     }); 
-});
+    it("should display orders", async () => {
+        render(<App />);
+        const orderOne = await waitFor(() => screen.getByText("Pat"));
+        const orderTwo = await waitFor(() => document.getElementById(2));
+        const orderThree = await waitFor(() => document.getElementById(3));
+
+        expect(orderOne).toBeInTheDocument();
+        // expect(orderTwo).toBeInTheDocument();
+        // expect(orderThree).toBeInTheDocument();
+    });
+}); 
 
